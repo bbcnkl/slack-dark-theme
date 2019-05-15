b3b2011e06aee217f7b99c55902945dbd3676221
Nikola Babic <bbcnkl@gmail.com>
2019-05-15 14:58:07 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,54 +4,32 @@ DEST_FILE="/usr/lib/slack/resources/app.asar.unpacked/src/static/ssb-interop.js"
 THEMES=("default" "one_dark" "low_contrast" "navy" "hot_dog_stand")
 
 usage() {
-  cat << EOF
-  Usage:
-	  $0 -t <$(echo "${THEMES[@]}" | tr " " "|")>
- 	  $0 -u
-   -t: theme to install.
-  -u: revert to the default Slack theme.
-  Note: You will have to re-run this script whenever you upgrade Slack.
-  EOF
-  exit 1
+	cat << EOF
+	Usage:
+		$0 -t <$(echo "${THEMES[@]}" | tr " " "|")>
+		$0 -u
+	-t: theme to install.
+	-u: revert to the default Slack theme.
+	Note: You will have to re-run this script whenever you upgrade Slack.
+	EOF
+	exit 1
 }
 
 uninstall_theme() {
 	for file in $DEST_FILE1 $DEST_FILE2
 	do
-		if [[ $(grep -c "CUSTOM THEMES CONFIG" $DEST_DIR/$file) -gt 0 ]]
+		if [[ $(grep -c "CUSTOM THEMES CONFIG" $DEST_FILE) -gt 0 ]]
 		then
-			sed -i '' -e '/^\/\/ CUSTOM THEMES CONFIG$/,$d' $DEST_DIR/$file
+			sed -i '' -e '/^\/\/ CUSTOM THEMES CONFIG$/,$d' $DEST_FILE
 		fi
 	done
 }
 
 install_theme() {
- curl https://raw.githubusercontent.com/bbcnkl/slack-dark-theme/master/fix.js >> $DEST_FILE
+	curl https://raw.githubusercontent.com/bbcnkl/slack-dark-theme/master/fix.js >> $DEST_FILE
 }
 
-while getopts "t:u" o; do
-  case "${o}" in
-    t)
-			t="${OPTARG}"
-			if [[ $(echo "${THEMES[@]}" | grep -o "$t" | wc -w) -gt 0 ]]
-			then
-				uninstall_theme
-      	install_theme "$t"
-			else
-				echo "Theme not found!"
-				echo
-				usage
-			fi
-      ;;
-    u)
-			t="original"
-      uninstall_theme
-      ;;
-    *)
-      usage
-      ;;
-  esac
-done
+install_theme()
 
 if [[ "$#" -eq 0 ]]; then usage; fi
 if [[ -z "$t" ]]; then usage; fi

59fac002635f2cb71557da4b5491721438c2a87c
Nikola Babic <bbcnkl@gmail.com>
2019-05-15 15:00:40 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,15 +4,13 @@ DEST_FILE="/usr/lib/slack/resources/app.asar.unpacked/src/static/ssb-interop.js"
 THEMES=("default" "one_dark" "low_contrast" "navy" "hot_dog_stand")
 
 usage() {
-	cat << EOF
-	Usage:
-		$0 -t <$(echo "${THEMES[@]}" | tr " " "|")>
-		$0 -u
-	-t: theme to install.
-	-u: revert to the default Slack theme.
-	Note: You will have to re-run this script whenever you upgrade Slack.
-	EOF
-	exit 1
+	echo "Usage:"
+	#	$0 -t <$(echo "${THEMES[@]}" | tr " " "|")>
+	#	$0 -u
+	echo "-t: theme to install."
+	echo "${THEMES[@]}"
+	echo "-u: revert to the default Slack theme."
+	echo "Note: You will have to re-run this script whenever you upgrade Slack."
 }
 
 uninstall_theme() {
@@ -29,10 +27,11 @@ install_theme() {
 	curl https://raw.githubusercontent.com/bbcnkl/slack-dark-theme/master/fix.js >> $DEST_FILE
 }
 
-install_theme()
 
 if [[ "$#" -eq 0 ]]; then usage; fi
 if [[ -z "$t" ]]; then usage; fi
+
+install_theme()
 
 echo "Restart Slack for changes to take effect."
 exit 0
